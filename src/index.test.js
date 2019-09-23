cb88e4b19f00ddf315028795920260c7173833d3
danielrajaei <daniel.rajaie@yahoo.com>
2019-09-24 02:35:50 +0330

    update test

--- src/index.test.js
+++ src/index.test.js
@@ -6,4 +6,6 @@ test('number check function', () => {
   expect(persianNumberToText.getText(1)).toBe('یک'); 
   expect(persianNumberToText.getText('1')).toBe('یک');
   expect(persianNumberToText.getText('a1')).toBeUndefined(); 
+  persianNumberToText.changeConfig({moneyOutput : true , moneyUnit: 'تومان'});
+  expect(persianNumberToText.getText('1000000')).toBe('یک میلیون تومان');
 });
