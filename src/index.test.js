769a2c3efc16924afa2cf877bf128c89acde8ef0
danielrajaei <daniel.rajaie@yahoo.com>
2019-10-06 02:23:31 +0330

    new features

--- src/index.test.js
+++ src/index.test.js
@@ -1,11 +1,12 @@
-const persianNumberToText = require('./index');
+NumberToPersianText  = require('./index')
 
 test('number check function', () => {
-  expect(persianNumberToText.getText('a1')).toBe('aیک');
-  persianNumberToText.changeConfig({ inputValidation: true });
-  expect(persianNumberToText.getText(1)).toBe('یک'); 
-  expect(persianNumberToText.getText('1')).toBe('یک');
-  expect(persianNumberToText.getText('a1')).toBeUndefined(); 
-  persianNumberToText.changeConfig({moneyOutput : true , moneyUnit: 'تومان'});
-  expect(persianNumberToText.getText('1000000')).toBe('یک میلیون تومان');
+  expect(NumberToPersianText.getText(1)).toBe('یک'); 
+  expect(NumberToPersianText.getText('a1')).toBe('aیک');
+  NumberToPersianText.changeConfig({ inputValidation: true });
+  expect(NumberToPersianText.getText(1)).toBe('یک'); 
+  expect(NumberToPersianText.getText('1')).toBe('یک');
+  expect(NumberToPersianText.getText('a1')).toBeUndefined(); 
+  NumberToPersianText.changeConfig({moneyOutput : true , moneyUnit: 'تومان'});
+  expect(NumberToPersianText.getText('1000000')).toBe('یک میلیون تومان');
 });
