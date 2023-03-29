c62371ccbdfa697a1699ffc44f4fc26c31da479c
Lion <lion@lion.com>
2023-03-29 17:10:02 +0430

    Add test for percent mode

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,8 @@
 NumberToPersianText  = require('./index')
 
 test('number check function', () => {
-  expect(NumberToPersianText.getText(1)).toBe('یک'); 
+  expect(NumberToPersianText.getText('-23.34%')).toBe('منفی بیست و سه ممیز سی و چهار صدم درصد');
+  expect(NumberToPersianText.getText(1)).toBe('یک');
   expect(NumberToPersianText.getText('a1')).toBe('aیک');
   NumberToPersianText.changeConfig({ inputValidation: true });
   expect(NumberToPersianText.getText(1)).toBe('یک'); 
